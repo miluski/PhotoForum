58806b4844449f3c732e1b67b639b4f77dc80f28
Maksymilian Sowula <50271575+miluski@users.noreply.github.com>
2025-03-23 19:01:00 +0100

    feat: Add app routes with interceptor and guard

---
+++ frontend - angular/src/app/unauthorized/unauthorized.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unauthorized',
+  templateUrl: './unauthorized.component.html',
+  standalone: false
+})
+export class UnauthorizedComponent {
+
+}
---
+++ frontend - angular/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl: 'https://78.88.231.247:4443/api/v1',
+  production: false,
+};
