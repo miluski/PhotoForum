ac2e772be64c46bc125470e4dc1c6e47f44f5b1b
Pawel <webloopbox@gmail.com>
2025-03-31 12:22:43 +0200

    update registration API endpoint and enhance global styles for better layout

--- frontend - next.js/hooks/mutations/register.mutation.ts
+++ frontend - next.js/hooks/mutations/register.mutation.ts
@@ -5,7 +5,7 @@ import { RegisterData } from "@/interfaces/auth";
 
 const register = async (data: RegisterData): Promise<any> => {
   try {
-    const response = await axiosInstance.post("/api/v1/auth/register", data);
+    const response = await axiosInstance.post("/auth/register", data);
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
